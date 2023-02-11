b61ee3b51e0cc2a74692d25ec2f33f6d95a43343
Devon Carew <devoncarew@gmail.com>
2023-02-10 21:16:42 -0800

    initial commit

---
+++ bin/pkgs.dart
@@ -0,0 +1,5 @@
+import 'package:pkgs/pkgs.dart' as pkgs;
+
+void main(List<String> arguments) {
+  print('Hello world: ${pkgs.calculate()}!');
+}
---
+++ test/pkgs_test.dart
@@ -0,0 +1,8 @@
+import 'package:pkgs/pkgs.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
